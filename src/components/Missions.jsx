e8ef90bf99b3c5f3a85a640c7dd990dcef0ac397
Ali zaib <129104458+AlizaibWEBdev@users.noreply.github.com>
2024-06-09 11:31:28 +0500

    update css 3

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -5,7 +5,7 @@ import Cliam from '../components/Cliam';
 import { Link } from 'react-router-dom';
 
 
-const MissionLink = ({ missionName, reward, link = false, id,setcoins =()=>{} }) => {
+const MissionLink = ({ missionName, reward, link = false, id, setcoins = () => { } }) => {
     const api = useApi();
 
 
@@ -15,13 +15,13 @@ const MissionLink = ({ missionName, reward, link = false, id,setcoins =()=>{} })
 
 
     const handelokClick = () => {
-        api.getUserCoins("ikDoteen").then((e)=>{
+        api.getUserCoins("ikDoteen").then((e) => {
             setcoins(e.data.coins);
             setshowclaimPopup(false);
         })
-        
-        
-       
+
+
+
     }
 
 
@@ -45,22 +45,26 @@ const MissionLink = ({ missionName, reward, link = false, id,setcoins =()=>{} })
 
 
 
-        <div target={link && '_blank'} className='mission center'>
+        <div target={link && '_blank'} className='mission center' style={{flexDirection:"column"}}>
+            <a target='_blank' onClick={async (e) => {
+                if (link) {
+                    e.preventDefault();
+                    await api.markMissionCompleted("userid", missionName);
+                    setCompleated(true);
+
+                    window.open(link, '_blank');
+
+                }
+            }}
+
+            >{missionName}</a>
+
+
+
             <div className="single-side center">
                 <img src="task.png" alt="" />
                 <div className="col">
-                    <a target='_blank' onClick={async (e) => {
-                        if (link) {
-                            e.preventDefault();
-                            await api.markMissionCompleted("userid", missionName);
-                            setCompleated(true);
 
-                            window.open(link, '_blank');
-
-                        }
-                    }}
-
-                    >{missionName}</a>
                     <div className="reward">
                         <img src="coin.png" alt="" />
                         <b>{reward}</b>
@@ -112,8 +116,8 @@ const MissionLink = ({ missionName, reward, link = false, id,setcoins =()=>{} })
                     </div>
                 </div>
             </div>
-            
-          <span>{">"}</span>
+
+            <span>{">"}</span>
 
             {compleated && !claimed && <button onClick={async (e) => {
                 e.preventDefault();
@@ -140,7 +144,7 @@ const MissionLink = ({ missionName, reward, link = false, id,setcoins =()=>{} })
 
 }
 
-const Missions = ({setcoins}) => {
+const Missions = ({ setcoins }) => {
     return (
         <div className='mission-wraper'>
             <div id="popup-claim--">
@@ -152,10 +156,10 @@ const Missions = ({setcoins}) => {
                 <MissionLink missionName="Join Kross Blockchain Community" reward="200,000" id={"m3"} />
                 <MissionLink missionName="Join Hashgreed Community" reward="200,000" id={"m4"} />
                 <MissionLink missionName="Join BUCCON Community" reward="200,000" id={"m5"} />
-                <MissionLink missionName="Follow Kross Blockchain on X" reward="300,000" link={"ttps://x.com/krosscoin_team"} id={"m6"}  setcoins={setcoins}/>
-                <MissionLink missionName="Follow Hashgreed on X" reward="300,000" link={"https://x.com/hashgreed"} id={"m7"}  setcoins={setcoins}/>
-                <MissionLink missionName="Follow BUCCON on X" reward="300,000" link={"https://x.com/buccon_org"} id={"m8"}  setcoins={setcoins}/>
-                <MissionLink missionName="Follow Kross Blockchain on Facebook" reward="400,000" link={"https://facebook.com/krosscoin"} id={"m9"} setcoins={setcoins}  />
+                <MissionLink missionName="Follow Kross Blockchain on X" reward="300,000" link={"ttps://x.com/krosscoin_team"} id={"m6"} setcoins={setcoins} />
+                <MissionLink missionName="Follow Hashgreed on X" reward="300,000" link={"https://x.com/hashgreed"} id={"m7"} setcoins={setcoins} />
+                <MissionLink missionName="Follow BUCCON on X" reward="300,000" link={"https://x.com/buccon_org"} id={"m8"} setcoins={setcoins} />
+                <MissionLink missionName="Follow Kross Blockchain on Facebook" reward="400,000" link={"https://facebook.com/krosscoin"} id={"m9"} setcoins={setcoins} />
                 <MissionLink missionName="Subscribe to Krosscoin Youtube Channel" reward="500,000" link={"https://youtube.com/@krosscoinproject6568?si=HfzPAESykCLs4bDk"} id={"m10"} setcoins={setcoins} />
 
             </div>
